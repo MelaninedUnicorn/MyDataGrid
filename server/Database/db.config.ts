006e2e5e478671298938d6f64ca017e6c8933310
Mugisha J Kakou <mugisha@joycava.com>
2021-12-01 16:18:21 +0100

    added my product-api dump and hosted my postgre database on heroku for presentation purposes

--- server/Database/db.config.ts
+++ server/Database/db.config.ts
@@ -1,12 +1,14 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-	user: "me",
-	host: "localhost",
-	database: "api",
-	password: "password",
+	user: "pyaoiwokrmharm",
+	host: "ec2-176-34-105-15.eu-west-1.compute.amazonaws.com",
+	database: "d36ubjpia1ldn1",
+	password: "5fb6af2830fe96942ab9a89ddfcf307b68c52ab52bf35dbe074c6801b150e0bc",
 	port: 5432,
+	ssl: {
+        rejectUnauthorized: false,
+    }
 });
-
 
 export default pool;
